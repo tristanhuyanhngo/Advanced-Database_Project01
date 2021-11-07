363858c2717d60acef9bbf18be48ed425bd4fdf7
wander23 <57674214+wander23@users.noreply.github.com>
2021-11-07 21:54:00 +0700

    chỉnh sửa
    mm Lan

--- Source/Project 01/Query.sql
+++ Source/Project 01/Query.sql
@@ -16,12 +16,13 @@ SELECT * FROM SanPham SP
 WHERE SP.SoLuongTon < 100
 
 ----Cho danh sach cac san pham ban chay nhat (so luong ban nhieu nhat)
-SELECT SP.MaSP, SP.TenSP FROM SanPham SP
-WHERE SP.SoLuongTon = (SELECT MIN(SoLuongTon) FROM SanPham)
-GROUP BY SP.MaSP, SP.TenSP
+SELECT CT.MaSP,SP.TenSP,  SUM(CT.SoLuong) AS SoLuong
+FROM dbo.CT_HoaDon CT JOIN dbo.SanPham SP ON SP.MaSP = CT.MaSP 
+GROUP BY  CT.MaSP, SP.TenSP
+ORDER BY SUM(CT.SoLuong) DESC 
 
 ----Cho danh sach cac san pham co doanh thu cao nhat
-SELECT TOP 5 SP.MaSP, SP.TenSP, SUM(CTHD.ThanhTien) AS DoanhThu
-FROM SanPham SP, CT_HoaDon CTHD
-WHERE SP.MaSP = CTHD.MaSP
+SELECT SP.MaSP, SP.TenSP, SUM(CTHD.ThanhTien) AS DoanhThu
+FROM SanPham SP JOIN CT_HoaDon CTHD ON SP.MaSP = CTHD.MaSP
 GROUP BY SP.MaSP, SP.TenSP
+ORDER BY SUM(CTHD.ThanhTien) DESC 
